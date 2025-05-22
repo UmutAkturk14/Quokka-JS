fe817af83506235915d729f3f89cad5c63bf8fe5
Umut Akturk <umut.akturk14@gmail.com>
2025-05-23 02:38:49 +0300

    Navbar done

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,10 +5,10 @@ import Quokka from "@assets/Quokka";
 
 const Navbar: React.FC<NavbarProps> = () => {
   return (
-    <div className="w-full 0">
+    <div className="w-full bg-yellow-50/30 backdrop-blur-sm border-b border-gray-100">
       <div className="w-3/4 h-20 m-auto flex justify-between items-center">
         <div className="flex gap-2 justify-center items-center">
-          <Quokka className="w-15 h-15 stroke-3 stroke-amber-500 text-sky-950" />
+          <Quokka className="w-14 h-14" />
           <p
             className="text-3xl font-bold"
             style={{ fontFamily: "Ancizar Serif" }}
@@ -16,21 +16,28 @@ const Navbar: React.FC<NavbarProps> = () => {
             Quokka JS
           </p>
         </div>
-        <div id="navbar-buttons" className="flex gap-6">
-          <Button variant="primary" onClick={() => alert("Clicked!")}>
+        <div id="navbar-buttons" className="flex gap-4">
+          <Button variant="basic" onClick={() => alert("Clicked!")}>
             Features
           </Button>
-          <Button variant="primary" onClick={() => alert("Clicked!")}>
+          <Button variant="basic" onClick={() => alert("Clicked!")}>
             Examples
           </Button>
-          <Button variant="primary" onClick={() => alert("Clicked!")}>
+          <Button variant="basic" onClick={() => alert("Clicked!")}>
             Docs
           </Button>
-          <Button variant="primary" onClick={() => alert("Clicked!")}>
+          <Button variant="basic" onClick={() => alert("Clicked!")}>
             GitHub
           </Button>
         </div>
-        <div id="navbar-cta-buttons">Cta buttons</div>
+        <div id="navbar-cta-buttons" className="flex gap-6">
+          <Button variant="outlined" onClick={() => alert("Clicked!")}>
+            npm install quokka-js
+          </Button>
+          <Button variant="call-to-action" onClick={() => alert("Clicked!")}>
+            Get Started
+          </Button>
+        </div>
       </div>
     </div>
   );
